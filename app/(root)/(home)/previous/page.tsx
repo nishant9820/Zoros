082238ed204e761fc02b88300b3c2bd814c6689d
Nishant Desai <you@example.com>
2024-04-27 09:53:53 +0530

    first commit

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const previous = () => {
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <h1 className="text-3xl font-extrabold lg:text-3xl">Previous</h1>
+    </section>
+  );
+};
+
+export default previous;
---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const upcoming = () => {
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <h1 className="text-3xl font-extrabold lg:text-3xl">Upcomings</h1>
+    </section>
+  );
+};
+
+export default upcoming;
